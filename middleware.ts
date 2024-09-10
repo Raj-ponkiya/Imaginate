ec0a255fe1429b3b721985e132d3a3c6076142aa
RajPonkiya <ponkiyaraj7@gmail.com>
2024-09-10 19:53:42 +0530

    initial sidebar

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,7 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
 export default clerkMiddleware();
-// const isPublicRoute = createRouteMatcher(["api/webhooks/clerk"])
 
-// export default clerkMiddleware((auth, request) => {
-  // if (!isPublicRoute(request)) {
-  //   auth().protect()
-  // }
-// });
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
